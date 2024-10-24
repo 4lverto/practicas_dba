fcfe3fc029cc37507e8f374776b06f82e32b55f1
migue-maca <migue.inginfo@gmail.com>
2024-10-24 15:54:55 +0200

    chore: initial project setup with basic configuration

---
+++ p2/src/p2/P2.java
@@ -0,0 +1,18 @@
+
+
+package p2;
+
+/**
+ *
+ * @author migue-maca
+ */
+public class P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
