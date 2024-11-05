69f12b8c16e35646ce913039bcf885e93bc16f55
migue-maca <migue.inginfo@gmail.com>
2024-11-05 14:05:50 +0100

    faena (Main): realizar varias pruebas para comprobar el funcionamiento esperado del entorno y los sensores

--- p2/src/app/Main.java
+++ p2/src/app/Main.java
@@ -1,107 +1,89 @@
 package app;
 
-import controlador.Controlador;
+
 import java.io.IOException;
-import modelo.Mapa;
+import java.util.ArrayList;
+import modelo.Entorno;
 import modelo.Posicion;
-import modelo.agentes.Agente;
+import modelo.sensores.Energia;
+import modelo.sensores.Sensor;
 import modelo.sensores.Vision;
-import vista.Vista;
-import vista.VistaTexto;
+
 
 /**
  * @brief Clase que ejecuta el simulador.
  */
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-//        Mapa mapa = new Mapa("mapas/mapWithComplexObstacle2.txt");
-//        
-//        mapa.mostrarMapa();
-//        
-//        // Prueba del nuevo método para establecer la casilla:
-//        mapa.establecerCasilla(1, 2, 1);
-//        System.out.println();
-//        mapa.mostrarMapa();
-//        
-//        // Prueba del sensor de la visión:
-//        Posicion posInicial     = new Posicion(2, 3);
-//        Vision vision           = new Vision(mapa, posInicial);
-//        int[][] celdasContiguas = vision.obtenerVision();
-//        
-//        System.out.println();
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                System.out.print(celdasContiguas[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//        // Cambiar posición del agente (en una zona del mapa con todas las zonas 
-//        // contiguas dentro de los límites del mapa):
-//        vision.actualizarPosAgente(new Posicion(5, 3));
-//        vision.actualizar();
-//        celdasContiguas = vision.obtenerVision();
-//        
-//        System.out.println();
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                System.out.print(celdasContiguas[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//        // Cambiar posición del agente (en una zona del mapa con todas las zonas 
-//        // contiguas superiores fuera de los límites del mapa):
-//        vision.actualizarPosAgente(new Posicion(0, 2));
-//        vision.actualizar();
-//        celdasContiguas = vision.obtenerVision();
-//        
-//        System.out.println();
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                System.out.print(celdasContiguas[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-
-//        // Prueba de la vista textual (por terminal):
-//        Mapa mapa = new Mapa("mapas/mapWithComplexObstacle2.txt");
-//        Vista vistaTexto = new VistaTexto();
-//        vistaTexto.actualizar(mapa);
-
-
-
-        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
-        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
-        // El programa se usaría de la siguiente manera (en principio):
-        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
-        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+    
+    public static void pruebasEntornoYSensores() throws IOException {
+        Posicion posAgente   = new Posicion(2, 3);
+        Posicion posObjetivo = new Posicion(7,1);
+        Entorno entorno      = Entorno.obtenerInstancia(posAgente, posObjetivo);
         
-        // 1. Crear el agente:
+        entorno.establecerMapa("mapas/mapWithComplexObstacle2.txt");
+        entorno.obtenerMapa().mostrarMapa();
         
-        // Suponiendo que son posiciones válidas:
-        Posicion posAgente   = new Posicion(2, 4);
-        Posicion posObjetivo = new Posicion(8, 0);
-        Agente agente        = new Agente(
-                posAgente, 
-                posObjetivo,
-                "mapas/mapWithComplexObstacle2.txt");
+        Energia energia = new Energia(entorno);
+        Vision vision   = new Vision(entorno);
         
-        // 2. Crear el controlador:
-        Controlador controlador = new Controlador(agente);
-        
-        // 3. Crear y registrar las vistas:
-        controlador.registrarVista(new VistaTexto());
-        
-        // 4. Simular la partida:
-        controlador.iniciarSimulacion();
-        
-        // Mientras no se alcance el objetivo, seguir simulando:
-        while (!controlador.objetivoAlcanzado()) {
-            controlador.ejecutar();
+        // Estado inicial:
+        System.out.println();
+        System.out.println("Sensores iniciales: ");
+        System.out.println("Sensor de energía: " + 
+                energia.obtenerEnergia());
+        System.out.println("Sensor de visión:");
+        int[][] v = vision.obtenerVision();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(v[i][j] + " ");
+            }
+            System.out.println();
         }
         
-        // 5. En este punto, el agente ha llegado a la casilla objetivo:
-        controlador.finalizarSimulacion();
+        // Siguiente estado:
+        Posicion pos = new Posicion(2, 4);
+        ArrayList<Sensor> sensores = 
+                entorno.actualizarPercepciones(pos);
+        System.out.println();
+        entorno.obtenerMapa().mostrarMapa();
+        
+        System.out.println();
+        System.out.println("Sensores: ");
+        System.out.println("Sensor de energía: " + 
+                energia.obtenerEnergia());
+        System.out.println("Sensor de visión:");
+        vision = (Vision) sensores.get(1);
+        v      = vision.obtenerVision();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(v[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        // Siguiente estado:
+        Posicion otraPos = new Posicion(3, 4);
+        sensores = entorno.actualizarPercepciones(otraPos);
+        System.out.println();
+        entorno.obtenerMapa().mostrarMapa();
+        
+        System.out.println();
+        System.out.println("Sensores: ");
+        System.out.println("Sensor de energía: " + 
+                energia.obtenerEnergia());
+        System.out.println("Sensor de visión:");
+        vision = (Vision) sensores.get(1);
+        v      = vision.obtenerVision();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(v[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        pruebasEntornoYSensores();
     }
     
 }
