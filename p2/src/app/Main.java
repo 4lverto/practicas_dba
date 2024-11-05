b3c24fd0d9124e81edfb03a6adcc4ce6ea6534ce
migue-maca <migue.inginfo@gmail.com>
2024-11-05 21:23:20 +0100

    logro (VistaGrafica): implementar la vista gráfica del simulador. Se crea una ventana principal, la cual contiene un panel con la representación del mapa. Como cada elemento del mapa tiene su entero a modo de identificación, se utiliza esto para pintar de un color u otro cada elemento

--- p2/src/app/Main.java
+++ p2/src/app/Main.java
@@ -9,6 +9,7 @@ import modelo.Posicion;
 import modelo.sensores.Energia;
 import modelo.sensores.Sensor;
 import modelo.sensores.Vision;
+import vista.VistaGrafica;
 import vista.VistaTexto;
 
 
@@ -26,35 +27,74 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //pruebasEntornoYSensores();
         //pruebaVistaTextual();
+        pruebaVistaGrafica();
         
         
         
-        // Crear las posiciones del agente y de la casilla objetivo (hay que 
-        // tener cuidado de que sean válidas para el mapa elegido):
-        Posicion posAgente   = new Posicion(2, 4);
-        Posicion posObjetivo = new Posicion(7, 1);
-        
-        // Crear el entorno para pasarlo al controlador:
-        Entorno entorno = Entorno.obtenerInstancia(
-                posAgente, 
-                posObjetivo);
-        // Establecer el mapa:
-        entorno.establecerMapa("mapas/mapWithComplexObstacle2.txt");
-        
-        // Crear el controlador, el cual lanzará al agente (el entorno le llega
-        // al agente a través del controlador, con las posiciones y el mapa 
-        // debidamente inicializados):
-        Controlador controlador = new Controlador(
-                "Agente-DBA-P2",
-                "modelo.agentes.Agente",
-                entorno);
-        
-        // Iniciar la simulación. Esta terminará cuando el agente llegue a la 
-        // casilla objetivo (el agente lo controla internamente).
-        controlador.ejecutar();
+//        // Crear las posiciones del agente y de la casilla objetivo (hay que 
+//        // tener cuidado de que sean válidas para el mapa elegido):
+//        Posicion posAgente   = new Posicion(2, 4);
+//        Posicion posObjetivo = new Posicion(7, 1);
+//        
+//        // Crear el entorno para pasarlo al controlador:
+//        Entorno entorno = Entorno.obtenerInstancia(
+//                posAgente, 
+//                posObjetivo);
+//        // Establecer el mapa:
+//        entorno.establecerMapa("mapas/mapWithComplexObstacle2.txt");
+//        
+//        // Crear el controlador, el cual lanzará al agente (el entorno le llega
+//        // al agente a través del controlador, con las posiciones y el mapa 
+//        // debidamente inicializados):
+//        Controlador controlador = new Controlador(
+//                "Agente-DBA-P2",
+//                "modelo.agentes.Agente",
+//                entorno);
+//        
+//        // Iniciar la simulación. Esta terminará cuando el agente llegue a la 
+//        // casilla objetivo (el agente lo controla internamente).
+//        controlador.ejecutar();
     }
     
     
+    
+    public static void pruebaVistaGrafica() throws IOException {
+        Posicion posAgente   = new Posicion(2, 3);
+        Posicion posObjetivo = new Posicion(7,1);
+        Entorno entorno      = Entorno.obtenerInstancia(posAgente, posObjetivo);
+        
+        entorno.establecerMapa("mapas/mapWithComplexObstacle2.txt");
+        
+        VistaGrafica vista = new VistaGrafica(entorno);
+        
+        // Pequeña simulación de prueba:
+        int i = 1;
+        
+        while (i <= 5) {
+            try {
+                Thread.sleep(1500); // Pausar segundo y medio.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            // Cambio en el entorno:
+            entorno.actualizarPercepciones(
+                    new Posicion(
+                            entorno.obtenerPosAgente().obtenerX(),
+                            entorno.obtenerPosAgente().obtenerY() + 1));
+            
+            i ++;
+        }
+        
+//        // Cambio en el entorno:
+//        Posicion pos = new Posicion(2, 4);
+//        entorno.actualizarPercepciones(pos);
+//        
+//        // Cambio en el entorno:
+//        pos = new Posicion(2, 5);
+//        System.out.println();
+//        entorno.actualizarPercepciones(pos);
+    }
     
     public static void pruebasEntornoYSensores() throws IOException {
         Posicion posAgente   = new Posicion(2, 3);
---
+++ p2/src/vista/VistaGrafica.java
@@ -0,0 +1,69 @@
+
+package vista;
+
+import modelo.Entorno;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+
+/**
+ * @class VistaGrafica
+ * 
+ * @brief Clase que representa la vista gráfica del simulador. Esta utiliza la
+ * biblioteca Swing para representar el entorno (mapa, agente y casilla 
+ * objetivo).
+ */
+public class VistaGrafica extends Vista {
+    
+    /**
+     * @brief Ventana gráfica principal para el simulador.
+     */
+    private JFrame ventana;
+    
+    /**
+     * @brief Panel para el mapa a visualizar.
+     */
+    private PanelMapa panelMapa;
+
+    
+    
+    /**
+     * @brief Constructor por parámetro. Inicializa la vista y crea la ventana
+     * principal con una configuración por defecto.
+     * 
+     * @param entorno Instancia del entorno.
+     */
+    public VistaGrafica(Entorno entorno) throws IOException {
+        this.entorno = entorno;
+        // Añadirse al entorno (observado) como observador:
+        this.entorno.registrarVista(this);
+        
+        this.ventana   = new JFrame("Prática 2 de DBA");
+        this.panelMapa = new PanelMapa(entorno.obtenerMapa());
+        
+        // Configuración de la ventana principal:
+        int anchoVentana = 
+                entorno.obtenerMapa().obtenerNumColumnas() * PanelMapa.obtenerFactor();
+        int altoVentana  = 
+                entorno.obtenerMapa().obtenerNumFilas() * PanelMapa.obtenerFactor();
+        
+        this.ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.ventana.setLayout(new BorderLayout());
+        this.ventana.add(this.panelMapa, BorderLayout.CENTER);
+        this.ventana.setSize(anchoVentana, altoVentana);
+        this.ventana.pack();
+        this.ventana.setLocationRelativeTo(null); // Centra la ventana
+        this.ventana.setVisible(true);
+    }
+
+    /**
+     * @brief Actualiza la vista en función del estado del entorno.
+     * Implementación del método abstracto de Vista.
+     */
+    @Override
+    public void actualizar() {
+        this.panelMapa.establecerMapa(this.entorno.obtenerMapa());
+        this.panelMapa.repaint(); // Para repintar la pantalla.
+    }
+}
