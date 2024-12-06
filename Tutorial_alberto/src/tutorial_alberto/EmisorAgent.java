351f919217f5a0cad7c7aea342b9f1dbd4fa7d68
4lverto <kesolesito@gmail.com>
2024-12-06 18:42:17 +0100

    Tutorial finalizado

--- Tutorial_alberto/src/tutorial_alberto/EmisorAgent.java
+++ Tutorial_alberto/src/tutorial_alberto/EmisorAgent.java
@@ -14,10 +14,10 @@ public class EmisorAgent extends Agent{
         while(!finishConversation){
             switch (this.step){
                 case 0 -> {
-                    ACLMessage msg = new ACLMessage();
+                    ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
                     msg.addReceiver(new AID("alberto-receptor",AID.ISLOCALNAME));
-                    msg.setContent("E - 'Hola agente. Como esta el tio? :P");
-                    msg.setConversationId("conversacion-agradecimiento");
+                    msg.setContent("Hola receptor, como esta el tio? :P");
+                    msg.setConversationId("CONVERSACION-AGRADECIMIENTO");
                     send(msg);
                     this.step=1;
                 }
@@ -25,18 +25,34 @@ public class EmisorAgent extends Agent{
                 case 1 -> {
                     ACLMessage msg = blockingReceive();
 
-                    if(msg.getConversationId().equals("conversacion-agradecimiento")){
-                        System.out.println("El emisor recibe este mensaje como respuesta: \n->R - '" + msg.getContent() + "'");
-                        this.finishConversation = true;
-                        System.out.println("\nE-Fin de la conversacion");
+                    if(msg.getConversationId().equals("CONVERSACION-AGRADECIMIENTO")
+                            && msg.getPerformative() == ACLMessage.AGREE){
+                        
+                        ACLMessage respuesta=msg.createReply(ACLMessage.INFORM);
+                        respuesta.setContent("Estoy saludando usando una conversacion protocolizada");
+                        send(respuesta);
+                        step=2;
                     }else{
-                        System.out.println("\nError en el protocolo de conversacion");
+                        System.out.println("\nError en el protocolo de conversacion - step"+1);
                         doDelete();
+                    }
+                }
+                
+                case 2 -> {
+                    ACLMessage msg = blockingReceive();
+                    
+                    if(msg.getConversationId().equals("CONVERSACION-AGRADECIMIENTO")
+                            && msg.getPerformative() == ACLMessage.INFORM){
+                        
+                        System.out.println("El emisor ha recibido el siguiente mensaje: \n->R-'" + msg.getContent() + "'");
+                        finishConversation=true;
+                    }else{
+                        System.out.println("Error en el protocolo de conversacion - step"+2);
                     }
                 }
 
                 default -> {
-                    System.out.println("\nError en el protocolo de conversacion");
+                    System.out.println("\nError en el protocolo de conversacion - step "+2);
                     doDelete();
                 }
             }
@@ -64,6 +80,48 @@ protected void setup(){
     msg.setContent("Hola agente");
 
     send(msg);
+
+    doDelete();
+}*/
+
+// ////////////////////// //
+// SEGUNDA PARTE TUTORIAL //
+// ////////////////////// //
+
+/*
+@Override
+protected void setup(){
+
+    while(!finishConversation){
+        switch (this.step){
+            case 0 -> {
+                ACLMessage msg = new ACLMessage();
+                msg.addReceiver(new AID("alberto-receptor",AID.ISLOCALNAME));
+                msg.setContent("E - 'Hola agente. Como esta el tio? :P");
+                msg.setConversationId("conversacion-agradecimiento");
+                send(msg);
+                this.step=1;
+            }
+
+            case 1 -> {
+                ACLMessage msg = blockingReceive();
+
+                if(msg.getConversationId().equals("conversacion-agradecimiento")){
+                    System.out.println("El emisor recibe este mensaje como respuesta: \n->R - '" + msg.getContent() + "'");
+                    this.finishConversation = true;
+                    System.out.println("\nE-Fin de la conversacion");
+                }else{
+                    System.out.println("\nError en el protocolo de conversacion");
+                    doDelete();
+                }
+            }
+
+            default -> {
+                System.out.println("\nError en el protocolo de conversacion");
+                doDelete();
+            }
+        }
+    }
 
     doDelete();
 }*/
