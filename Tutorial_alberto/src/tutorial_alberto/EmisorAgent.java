fd115db1f76009d988f70c42063d109c92ce6552
4lverto <kesolesito@gmail.com>
2024-12-06 13:34:25 +0100

    Problemas con la segunda parte del tutorial. No hay intercambio de mensajes

--- Tutorial_alberto/src/tutorial_alberto/EmisorAgent.java
+++ Tutorial_alberto/src/tutorial_alberto/EmisorAgent.java
@@ -5,16 +5,39 @@ import jade.core.Agent;
 import jade.lang.acl.ACLMessage;
 
 public class EmisorAgent extends Agent{
+    private int step=0;
+    private boolean finishConversation=false;
+    
     @Override
     protected void setup(){
-        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-
-        msg.addReceiver(new AID("alberto-receptor",AID.ISLOCALNAME));
-        msg.setContent("Hola agente");
-        
-        send(msg);
-        
-        doDelete();
+        switch (this.step){
+            case 0 -> {
+                ACLMessage msg = new ACLMessage();
+                msg.addReceiver(new AID("alberto-receptor",AID.ISLOCALNAME));
+                msg.setContent("E - Hola agente. Como esta el tio? :P");
+                msg.setConversationId("conversacion-agradecimiento");
+                send(msg);
+                this.step=1;
+            }
+            
+            case 1 -> {
+                ACLMessage msg = blockingReceive();
+                
+                if(msg.getConversationId().equals("conversacion-agradecimiento")){
+                    System.out.println("El emisor recibe este mensaje como respuesta: '" + msg.getContent() + "'");
+                    this.finishConversation = true;
+                    System.out.println("\nFin de la conversacion");
+                }else{
+                    System.out.println("\nError en el protocolo de conversacion");
+                    doDelete();
+                }
+            }
+            
+            default -> {
+                System.out.println("\nError en el protocolo de conversacion");
+                doDelete();
+            }
+        }
     }
     
     @Override
@@ -23,3 +46,19 @@ public class EmisorAgent extends Agent{
     }
 }
 
+
+// /////////////////////// //
+// TUTORIAL INICIAL BÁSICO //
+// /////////////////////// //
+/*
+@Override
+protected void setup(){
+    ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+
+    msg.addReceiver(new AID("alberto-receptor",AID.ISLOCALNAME));
+    msg.setContent("Hola agente");
+
+    send(msg);
+
+    doDelete();
+}*/
--- Tutorial_alberto/src/tutorial_alberto/ReceptorAgent.java
+++ Tutorial_alberto/src/tutorial_alberto/ReceptorAgent.java
@@ -7,13 +7,13 @@ public class ReceptorAgent extends Agent{
     @Override
     protected void setup(){
         ACLMessage msg = blockingReceive();
-        //System.out.println(msg);
         
-        if(msg != null && msg.getPerformative() == ACLMessage.INFORM){
-            System.out.println("-Receptor: Mensaje recibdo: '" + msg.getContent() + "'");
-        }else{
-            System.out.println("Mensaje no entenido o no esperado");
-        }
+        System.out.println("\nEl receptor recibe este mensaje: '" + msg.getContent() + "'");
+        
+        ACLMessage respuesta = msg.createReply();
+        
+        respuesta.setContent("\nBien!!, que alegria verte :D");
+        send(respuesta);
         
         doDelete();
     }
@@ -24,3 +24,21 @@ public class ReceptorAgent extends Agent{
     }
 }
 
+
+// /////////////////////// //
+// TUTORIAL INICIAL BÁSICO //
+// /////////////////////// //
+/*
+@Override
+protected void setup(){
+    ACLMessage msg = blockingReceive();
+    //System.out.println(msg);
+
+    if(msg != null && msg.getPerformative() == ACLMessage.INFORM){
+        System.out.println("-Receptor: Mensaje recibdo: '" + msg.getContent() + "'");
+    }else{
+        System.out.println("Mensaje no entenido o no esperado");
+    }
+
+    doDelete();
+}*/
